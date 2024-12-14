38c19a931cd2060f697c6b514cd6d6585e858518
gityashh <yashrajput8359@gmail.com>
2024-12-14 15:37:32 +0530

    add attendance done

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,6 +4,7 @@ const dbConnect = () => {
     try{
         mongoose.connect(process.env.MONGODB_URL)
         console.log('database connected')
+        
     }catch(err){
         console.log(err)
     }
